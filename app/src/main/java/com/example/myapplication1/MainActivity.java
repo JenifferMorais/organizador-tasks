57820f9f92fef2d8780fbfb664471f4798d522e5
Jeniffer <jenimorais28@gmail.com>
2022-11-08 14:28:45 -0300

    começando parte 3

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -1,22 +1,43 @@
 package com.example.myapplication1;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.myapplication1.Utils.DataBaseHelper;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.myapplication1.databinding.ActivityMainBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
 private ActivityMainBinding binding;
 
+    private RecyclerView mRecyclerview;
+    private FloatingActionButton fab;
+    private DataBaseHelper myDB;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerview = findViewById(R.id.recyclerview);
+        fab = findViewById(R.id.fab);
+        myDB = new DataBaseHelper(MainActivity.this);
+
+    fab.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+        }
+    });
     }
 
 }
