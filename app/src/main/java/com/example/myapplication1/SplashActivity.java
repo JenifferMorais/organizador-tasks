5bc749ed28781effc8bef033c9af116eafeb997f
Jeniffer <jenimorais28@gmail.com>
2022-11-08 12:41:55 -0300

    commit inicial

--- app/src/main/java/com/example/myapplication1/SplashActivity.java
+++ app/src/main/java/com/example/myapplication1/SplashActivity.java
@@ -1,10 +1,7 @@
 package com.example.myapplication1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -12,14 +9,5 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(SplashActivity.this , MainActivity.class));
-                finish();
-            }
-        } , 4000);
     }
 }
