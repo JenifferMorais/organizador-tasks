db1a7ca562c5d0f8cc134bb97a17927bb5849955
Jeniffer <jenimorais28@gmail.com>
2022-12-06 20:19:22 -0300

    ultimas modificacoes

--- app/src/main/java/com/example/myapplication1/Adapter/ToDoAdapter.java
+++ app/src/main/java/com/example/myapplication1/Adapter/ToDoAdapter.java
@@ -84,7 +84,6 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.MyViewHolder> 
         AddNewTask task = new AddNewTask();
         task.setArguments(bundle);
         task.show(activity.getSupportFragmentManager() , task.getTag());
-        
 
     }
 
